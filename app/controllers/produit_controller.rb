4fe9367978c4612ca3b5dd53cdd21ff2a7a02876
David Poisson <zeraaiur@hotmail.com>
2019-02-27 16:55:15 -0500

    structure et scss pour le top de la page, un peu de controlleur

--- app/controllers/produit_controller.rb
+++ app/controllers/produit_controller.rb
@@ -1,5 +1,11 @@
 class ProduitController < ApplicationController
   def index
+    @enveloppes = Enveloppe.all
+
+    @faireparts = Produit.all.select { |m| m.typeproduit == "fairepart" }
+    @reponses = Produit.all.select { |m| m.typeproduit == "reponse" }
+    @invitations = Produit.all.select { |m| m.typeproduit == "invitation" }
+    @menus = Produit.all.select { |m| m.typeproduit == "menu" }
   end
 
   def show
