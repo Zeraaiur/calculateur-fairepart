c0fd73db61b93664e4a1bdd76fadc84d8aa70f73
David Poisson <zeraaiur@hotmail.com>
2019-02-28 16:10:17 -0500

    number form working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+function numberFieldValidate(e){
+  document.getElementById("filter-fairepart").click();
+}
--- app/controllers/produit_controller.rb
+++ app/controllers/produit_controller.rb
@@ -7,7 +7,13 @@ class ProduitController < ApplicationController
     @invitations = Produit.all.select { |m| m.typeproduit == "invitation" }
     @menus = Produit.all.select { |m| m.typeproduit == "menu" }
 
+    @mynombrefp = params[:mynombrefp] || 25
 
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
 
   end
 
